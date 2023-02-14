1b52762a2462bfca794688857810a8794e250201
Magomaanza <magomaanza@gmail.com>
2023-02-14 12:22:30 +0200

    i now see the light after the tunnel

--- js/index.js
+++ js/index.js
@@ -1,19 +1,18 @@
-// function myFunction(){
-//     let x = document.getElementById("myTopnav");
-//     if(x.className === "header"){
-//         x.className += " responsive";
-//     }else{
-//         x.className = "header";
-//     }
-// }
-const harmbuger = document.querySelector(".harmbuger");
-const navMenu= document.querySelector(".nav-menu");
+function myFunction(){
+    let x = document.getElementById("myTopnav");
+    if(x.className === "header"){
+        x.className += " responsive";
+    }else{
+        x.className = "header";
+    }
+}
 
-harmbuger.addEventListener("click", () =>{
-    harmbuger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
-document.querySelector(".nav-link").forEach(n => n.addEventListener("click",()=>{
-    harmbuger.classList.remove("active");
-    navMenu.classList.remove("active");
-}));
+
+function myFunction() {
+    var x = document.getElementsByClassName("menuItems");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
