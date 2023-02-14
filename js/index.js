c3c1daeff2d26e8a45ec00f943f95aca43df8fc7
Magomaanza <magomaanza@gmail.com>
2023-02-14 08:09:05 +0200

    not sure what to do anymore

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
-function myFunction(){
-    let x = document.getElementById("myTopnav");
-    if(x.className === "header"){
-        x.className += " responsive";
-    }else{
-        x.className = "header";
-    }
-}
+// function myFunction(){
+//     let x = document.getElementById("myTopnav");
+//     if(x.className === "header"){
+//         x.className += " responsive";
+//     }else{
+//         x.className = "header";
+//     }
+// }
+const harmbuger = document.querySelector(".harmbuger");
+const navMenu= document.querySelector(".nav-menu");
+
+harmbuger.addEventListener("click", () =>{
+    harmbuger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+document.querySelector(".nav-link").forEach(n => n.addEventListener("click",()=>{
+    harmbuger.classList.remove("active");
+    navMenu.classList.remove("active");
+}));
