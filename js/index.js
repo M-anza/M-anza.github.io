4fc67f11e6c73653cac1f6cb471637171521ac08
Magomaanza <magomaanza@gmail.com>
2023-02-15 09:24:31 +0200

    actave the harmburger menu

--- js/index.js
+++ js/index.js
@@ -1,18 +1,17 @@
-function myFunction(){
-    let x = document.getElementById("myTopnav");
-    if(x.className === "header"){
-        x.className += " responsive";
-    }else{
-        x.className = "header";
-    }
-}
+// function myFunction(){
+//     let x = document.getElementById("myTopnav");
+//     if(x.className === "header"){
+//         x.className += " responsive";
+//     }else{
+//         x.className = "header";
+//     }
+// }
 
-
-function myFunction() {
-    let x = document.getElementsByClassName("menuItems");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+let harmburgerMenu = document.getElementById("menu");
+harmburgerMenu.addEventListener("click",toggleMenu);
+function toggleMenu() {
+    let x = document.querySelector(".menuItems");
+    console.log(x.classList)
+     x.classList.toggle("active") 
+     
   }
